6180519a34c32245d24349ed10d235b8e1ac5861
Htet-Myark <HTETMYARK99.23@ichat.sp.edu.sg>
2026-08-30 23:36:50 +0800

    skill added

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,7 +4,7 @@ const skills = [
   'HTML', 'CSS', 'JavaScript', 'Python', 'Java', 'SQL',
   'React', 'React Native', 'Vite', 'Node.js', 'Express', 'REST API',
   'MySQL', 'PostgreSQL', 'UiPath', 'Power Automate', 'SharePoint','Katalon Studio',
-  'Docker', 'AWS Basics', 'CI/CD', 'Git',
+  'Docker', 'AWS Basics', 'CI/CD', 'Git','Kuberbetes Basics ','Linux Basics',
   'UI/UX Design', 'Data Analysis', 'Excel', 'Power BI', 'Secure Coding', 'Agile', 'C#', 'Figma','Selenium', 'Jira', 'Postman', 'MongoDB', 'Three.js', 'WebSockets', 'Azure Basics'
 ]
 
